8f7c16fd6639496697a3406d8b5088a93653bad7
Alaina Newmark <68250402+alaidriel@users.noreply.github.com>
2023-01-02 15:38:25 -0600

    feat: solve day 1

--- 2022/src/solutions/day01.rs
+++ 2022/src/solutions/day01.rs
@@ -2,8 +2,24 @@ use common::Solution;
 
 pub struct DayOne;
 
+const INPUT: &str = include_str!("../../input/day01.txt");
+
 impl Solution for DayOne {
     fn solve() {
-        println!("Hello, world!");
+        let input: Vec<_> = INPUT.trim().split("\n\n").collect::<Vec<_>>();
+        let mut calorie_counts: Vec<usize> = vec![];
+        for elf in &input {
+            let mut total_calories: usize = 0;
+            for calories in &elf.trim().split("\n").collect::<Vec<_>>() {
+                total_calories += calories.parse::<usize>().unwrap();
+            }
+            calorie_counts.push(total_calories);
+        }
+        calorie_counts.sort();
+        println!("Part One: {}", calorie_counts.last().unwrap());
+        println!(
+            "Part Two: {}",
+            calorie_counts.iter().rev().take(3).sum::<usize>()
+        );
     }
 }
