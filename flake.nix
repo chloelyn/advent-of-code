bdd1cc792f2fe6e2b550251bc03fd08ee5253705
Alaina Newmark <68250402+alaidriel@users.noreply.github.com>
2023-01-02 15:38:42 -0600

    feat: add support for `nix develop`

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay }: {
+    defaultPackage.x86_64-darwin =
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit overlays;
+          system = "x86_64-darwin";
+        };
+        rust-nightly = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+          extensions = [ "rust-src" ];
+        });
+      in
+      with pkgs;
+      stdenv.mkDerivation {
+        name = "advent-of-code";
+        src = self;
+        buildInputs = [ rust-nightly ];
+      };
+  };
+}
