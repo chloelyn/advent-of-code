ddb5c26f62c174d4a5ba5fe43a09427b1e441c3d
Alaina Newmark <68250402+alaidriel@users.noreply.github.com>
2023-01-02 15:38:38 -0600

    refactor(2021): clean up day 15 solution

--- 2021/src/solutions/day15.rs
+++ 2021/src/solutions/day15.rs
@@ -1,5 +1,7 @@
 use common::graph::Graph;
 
+const EXTENSION_FACTOR: usize = 5;
+
 pub fn input() -> &'static str {
     include_str!("../../input/day15.txt")
 }
@@ -15,7 +17,7 @@ pub fn solve(input: &str) -> (usize, usize) {
     let g = Graph::from(&weights);
     let part_one = g.dijkstra(0, g.nodes.len() - 1).unwrap();
 
-    let extended = extend(&weights, width, width * 5);
+    let extended = extend(&weights, width, width * EXTENSION_FACTOR);
     let g = Graph::from(&extended);
     let part_two = g.dijkstra(0, g.nodes.len() - 1).unwrap();
 
@@ -34,16 +36,17 @@ fn weights(input: &str) -> Vec<Vec<usize>> {
 }
 
 fn extend(initial: &Vec<Vec<usize>>, initial_dimensions: usize, height: usize) -> Vec<Vec<usize>> {
-    let mut weights: Vec<Vec<usize>> = vec![vec![0; initial.len() * 5]; initial.len() * 5];
+    let mut weights: Vec<Vec<usize>> =
+        vec![vec![0; initial.len() * EXTENSION_FACTOR]; initial.len() * EXTENSION_FACTOR];
     for c in 0..initial_dimensions {
-        let extended = extend_col(
+        let extended = extend_linear(
             &initial
                 .iter()
                 .enumerate()
                 .map(|(row, _)| initial[row][c])
                 .collect(),
             initial_dimensions,
-            5,
+            EXTENSION_FACTOR,
         );
         for r in 0..height {
             weights[r][c] = extended[r]
@@ -51,35 +54,18 @@ fn extend(initial: &Vec<Vec<usize>>, initial_dimensions: usize, height: usize) -
     }
     for r in 0..height {
         weights[r].truncate(initial_dimensions);
-        weights[r] = extend_row(&weights[r], initial_dimensions, 5);
+        weights[r] = extend_linear(&weights[r], initial_dimensions, EXTENSION_FACTOR);
     }
     weights
 }
 
-fn extend_row(initial: &Vec<usize>, width: usize, factor: usize) -> Vec<usize> {
+fn extend_linear(initial: &Vec<usize>, width: usize, factor: usize) -> Vec<usize> {
     let mut weights: Vec<usize> = initial.clone();
     let len = weights.len();
     for i in 0..factor - 1 {
         let offset = i * len;
         let w = weights.clone();
         let mapped = w[offset..offset + width]
-            .into_iter()
-            .map(|value| match value + 1 {
-                v @ 1..=9 => v,
-                _ => 1,
-            });
-        weights.extend(mapped);
-    }
-    weights
-}
-
-fn extend_col(initial: &Vec<usize>, height: usize, factor: usize) -> Vec<usize> {
-    let mut weights: Vec<usize> = initial.clone();
-    let len = weights.len();
-    for i in 0..factor - 1 {
-        let offset = i * len;
-        let w = weights.clone();
-        let mapped = w[offset..offset + height]
             .into_iter()
             .map(|value| match value + 1 {
                 v @ 1..=9 => v,
