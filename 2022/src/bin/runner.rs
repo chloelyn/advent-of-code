fe23366b2dde9f0bd2e7699bba9f37088c1686f9
Alaina Newmark <68250402+alaidriel@users.noreply.github.com>
2023-01-02 15:38:24 -0600

    feat: add cursed project structure

---
+++ 2022/src/bin/runner.rs
@@ -0,0 +1,6 @@
+use common::Solution;
+use twenty_two::solutions::*;
+
+fn main() {
+    day01::DayOne::solve();
+}
---
+++ 2022/src/solutions/day01.rs
@@ -0,0 +1,9 @@
+use common::Solution;
+
+pub struct DayOne;
+
+impl Solution for DayOne {
+    fn solve() {
+        println!("Hello, world!");
+    }
+}
